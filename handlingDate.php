397143430fee8ed6df2d1ece5404ddf95e839457
Алексей <Strannick-Sirius@yandex.ru>
2013-09-12 01:16:41 -0700

    Create handlingDate.php

---
+++ handlingDate.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* Alex_DB_Inteface_handlingDate
+*
+* Класс для работы с данными: выбрать, удалить, обновить.
+*
+* @author D_S <Strannick-Sirius@yandex.ru>
+* @version 1.0
+*/
+
+class Alex_DB_Inteface_handlingDate{
+	
+	/**
+	* Метод класса
+	*
+	* Выбор данных из объекта
+	*/
+	public function selectDate(){}
+	
+	/**
+	* Метод класса
+	*
+	* Создает данные в объекте
+	*/
+	public function createDate(){}
+	
+	/**
+	* Метод класса
+	*
+	* Удаляет данные из объекта
+	*/
+	public function deleteDate(){}
+	
+	/**
+	* Метод класса
+	*
+	* Обновляет данные в объекте
+	*/
+	public function updateDate(){}
+}
+
+?>
