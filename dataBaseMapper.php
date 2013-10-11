65035557545bdd37d9652ccdc3121597a6f0262b
Алексей <Strannick-Sirius@yandex.ru>
2013-10-11 03:45:56 -0700

    Create dataBaseMapper.php

---
+++ dataBaseMapper.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Alex_Db_Interface_dataBaseMapper
+ *
+ * Интерфейс для работы с БД
+ *
+ * @author D_S <Strannick-Sirius@yandex.ru>
+ * @version 1.0
+ */
+
+
+
+interface Alex_Db_Interface_dataBaseMapper{
+	
+	public $tableName;
+	
+	/**
+	 * Конструктор устанавливает соединение с БД
+	 * Принимает массив значений
+	 *
+	 * @param array $aConnect
+	 */
+	public function __construct(array $aConnect = array());
+	
+	/**
+	 * Обновляет данные в БД
+	 *
+	 * @param array $aValue
+	 * @param array $aWhereOptions
+	 */
+	public function update(array $aValue = array, array $aWhereOptions = NULL);
+	
+	/**
+	 * Добавляет данные БД
+	 *
+	 * @param array $aValue
+	 */
+	public function insert(array $aValue = array());
+	
+	/**
+	 * Выбирает данные из БД 
+	 *
+	 * @param array  $aCols
+	 * @param array  $aWhereOptions
+	 * @param array  $aOrderByOptions
+	 * @param string $sDescOptions
+	 */
+	public function select(array $aCols = array, array $aWhereOptions = NULL, array $aOrderByOptions = NULL, $sDescOptions = NULL);
+	
+	/**
+	 * Удаляет данные из таблицы
+	 *
+	 * @param array  $aOrderByOptions
+	 * @param array  $aWhereOptions
+	 * @param string $sDescOptions
+	 * @param int    $iLimitOptions
+	 */
+	public function delete(array $aOrderByOptions = NULL, array $aWhereOptions = NULL, $sDescOptions = NULL, $iLimitOptions = NULL);
+	
+	/**
+	 * Устанавливает имя БД
+	 *
+	 * @param string $sTableName
+	 */
+	public function setTable($sTableName);
+	
+	/**
+	 * Возвращает имя БД
+	 *
+	 * @return string $sTableName
+	 */
+	public function getTable();
+}
+?>
